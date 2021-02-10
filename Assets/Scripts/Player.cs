bea7f9c3920d58ea1960989ca6b14192824f1e44
HillerDavid <raiotech@gmail.com>
2021-02-10 08:19:48 -0500

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Take current position = new position (0, 0, 0)
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown())
+        transform.Translate(new Vector3(1f, 1f, 0) * _speed * Time.deltaTime);
+    }
+}
